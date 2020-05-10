7982a80e7d54efa56032c443e3536ec8c15e5f91
Gigas Taufan <gigastaufan@gmail.com>
2020-05-10 21:55:36 +0700

    using package

--- world_time_app/lib/pages/choose_location.dart
+++ world_time_app/lib/pages/choose_location.dart
@@ -7,31 +7,7 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-//  int counter = 0;
 
-//  this function is for asynchronous code
-  void getData() async {
-     // simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-//      print('Gas');
-        return 'Gas';
-    });
-
-    //  simulate network request to get bio of username
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-//      print('Mahasiswa');
-        return 'mahasiswa';
-    });
-
-    print('$username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-//    print('Init state running');
-    getData();
-  }
 
   @override
   Widget build(BuildContext context) {
