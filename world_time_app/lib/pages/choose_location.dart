56296171d8e3d4a1bef04de704aff6a2fc4192b3
Gigas Taufan <gigastaufan@gmail.com>
2020-05-10 11:09:39 +0700

    asynchronous function

--- world_time_app/lib/pages/choose_location.dart
+++ world_time_app/lib/pages/choose_location.dart
@@ -7,17 +7,34 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  int counter = 0;
+//  int counter = 0;
+
+//  this function is for asynchronous code
+  void getData() async {
+     // simulate network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+//      print('Gas');
+        return 'Gas';
+    });
+
+    //  simulate network request to get bio of username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+//      print('Mahasiswa');
+        return 'mahasiswa';
+    });
+
+    print('$username - $bio');
+  }
 
   @override
   void initState() {
     super.initState();
-    print('Init state running');
+//    print('Init state running');
+    getData();
   }
 
   @override
   Widget build(BuildContext context) {
-    print('Build state running $counter');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -27,12 +44,7 @@ class _ChooseLocationState extends State<ChooseLocation> {
         elevation: 0,
       ),
       body: FlatButton(
-        onPressed: (){
-          setState(() {
-            counter+=1;
-          });
-        },
-        child: Text('counter is $counter'),
+        onPressed: (){},
       ),
     );
   }
